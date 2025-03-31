db2895cc0d347fff245ff9a4a617219703c48796
sujit-sahu <S.K.Sahu@soton.ac.uk>
2025-03-31 16:56:16 +0100

    objectified resid

--- R/methods.R
+++ R/methods.R
@@ -237,34 +237,36 @@ message("\n Note that the residuals are provided on the transformed scale. Pleas
  }
  a 
 } 
+#' Extract residuals from a bmstdr  fitted object. 
+#' @param object A bmstdr model fit object. 
+#' @param ... Any other additional arguments. 
+## #' @seealso \link{\code{print}} for basic information regarding the fitted model, 
+## #' \link{\code{summary}} for summaries of model fitting,  
+## #' \link{\code{fitted}} for extracting the fitted values, 
+## #' \link{\code{plot}} for plotting. 
+#' @method resid bmstdr
+#' @rdname resid.bmstdr 
+#' @export
+resid.bmstdr <- function(object)
+{
+ if (object$scale.transform !="NONE") {  
+    message("\n Note that the residuals are provided on the transformed scale. 
+    See the scale.transform argument.\n")
+ }  
+  if (exists("stats", object)) { 
+   message("Validation has been performed.\n")
+   message("The residuals include the validation observations as well. 
+    Expect the return value to be of the same length as the supplied data frame. \n")
+ }
+  
+  a <- object$residuals
+  message("\nSummary of the residuals\n")
+  print(summary(a))
+  a 
+} 
 #' Is it a bmstdr model fitted object?
 #' @param x Any R object. 
 #' @return A TRUE/FALSE logical output.
 #' @export
 is.bmstdr <- function(x) inherits(x, "bmstdr")
 
-#' Extract residuals from a bmstdr  fitted object. 
-#' @param x A bmstdr model fit object. 
-## ## @seealso \link{\code{print}} for basic information regarding the fitted model, 
-## ## \link{\code{summary}} for summaries of model fitting,  
-## ## \link{\code{fitted}} for extracting the fitted values, 
-## ## \link{\code{plot}} for plotting.  
-#' @method resid bmstdr
-#' @export
-resid.bmstdr <- function(x)
-{
-  if (x$scale.transform !="NONE") {  
-    message("\n Note that the residuals are provided on the transformed scale. 
-    See the scale.transform argument.\n")
-  }  
-  if (exists("stats", x)) { 
-    message("Validation has been performed.\n")
-    message("The residuals include the validation observations as well. 
-    Expect the return value to be of the same length as the supplied data frame. \n")
-  }
-  
-  a <- x$residuals
-  message("\nSummary of the residuals\n")
-  print(summary(a))
-  a 
-} 
